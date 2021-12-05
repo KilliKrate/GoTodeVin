0d3da5717f0adc0df3728c33a2fe245563632dea
Davide <davidecavicchini@hotmail.com>
2021-12-05 09:58:11 +0100

    ho installato le librerie necessarie e importate

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
 
+var sqlite = require('sqlite3');
+const db = await sqlite.open({
+    filename: './NOME_DATABASE.db',
+    driver: sqlite3.Database
+});
+
+var cors = require('cors');
+app.use(cors());
+
+// module to parse the API body request
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
+});
+
+app.get('/catalogo/vini', function (req, res) {
+
 });
 
 app.listen(3000, function () {
