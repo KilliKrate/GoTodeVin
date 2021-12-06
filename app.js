34c398b5b4b7b7a4b0970ddd78cf654cdf4b9d69
Davide <davidecavicchini@hotmail.com>
2021-12-06 18:00:59 +0100

    creato api per il catalogo (tutti i vini, ricerca, dettaglio)

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ var express = require('express');
 var app = express();
 
 var sqlite = require('sqlite3');
-const db = await sqlite.open({
-    filename: './NOME_DATABASE.db',
-    driver: sqlite3.Database
-});
+const db = new sqlite.Database('./GoToDeDB.db');
 
 var cors = require('cors');
 app.use(cors());
@@ -21,8 +18,33 @@ app.get('/', function (req, res) {
 });
 
 app.get('/catalogo/vini', function (req, res) {
-
+    let sql = "SELECT nome FROM Vini"; //non devo inviarti tutto quello lo faccio nel dettaglio giusto?
+    db.all(sql, (err, rows) => {
+        res.send(rows);
+    })
 });
+
+app.get('/catalogo/ricerca', function (req, res) {
+    let sql = "SELECT * FROM Vini WHERE"; //assumo che questa funzione non possa essere chiamata senza almeno un criterio di ricerca
+
+    let nome=req.body['Name'], annata=req.body['Annata'];
+    if(nome!="") {
+        sql += " nome LIKE \"%"+nome+"%\"";
+        if(annata!="") sql += " AND";
+    }
+    if(annata!="") sql += " annata = "+annata;
+
+    db.all(sql, (err, rows) => {
+        res.send(rows);
+    })
+})
+
+app.get('/catalogo/dettaglio/:name', function (req, res) {
+    let sql = "SELECT * FROM Vini WHERE nome = \""+req.params.name+"\"";
+    db.all(sql, (err, rows) => {
+        res.send(rows);
+    })
+})
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
