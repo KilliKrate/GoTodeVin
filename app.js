5a5000a449e0ebd206b86dc6067147a6806bde09
KilliKrate <KilliKrate@users.noreply.github.com>
2021-12-06 20:50:47 +0100

    Aggiornato gitignore e porta di ascolto

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.get('/catalogo/vini', function (req, res) {
 
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+    console.log('GoTodeVin listening on port 8080!');
 });
