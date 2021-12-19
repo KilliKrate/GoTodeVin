3550689455b7dfe18e0df16b1a62226aa64d09d7
Angelox547 <angelox547@gmail.com>
2021-12-19 17:11:05 +0100

    API testing: Test API ricerca vino

--- test/index.js
+++ test/index.js
@@ -22,3 +22,17 @@ test('API 0: Catalog', function (assert) {
             assert.end();
         });
 });
+
+test('API 1: Search wine', function (assert) {
+    request(app)
+        .get('/api/catalogo/ricerca/Sauvignon&2016')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (err, res) {
+            var expectedResult = db.prepare(`SELECT nome,annata FROM Vini WHERE nome LIKE ? AND annata = ? AND disponibilita > 0`).all('%Sauvignon%',2016);
+
+            assert.error(err, 'No error');
+            assert.same(res.body, expectedResult, 'correct wine');
+            assert.end();
+        });
+});
